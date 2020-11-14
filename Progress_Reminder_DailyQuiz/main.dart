7a3d29c89fe024567c7f847384ded02aa876131c
shree675 <58718144+shree675@users.noreply.github.com>
2020-11-14 20:40:44 +0530

    Started implementing daily quiz section

---
+++ Progress_Reminder_DailyQuiz/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'homescreen.dart';
+
+main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
