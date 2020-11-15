571e94254c77c976220aaa2ad9fbac0493ef84e4
shree675 <58718144+shree675@users.noreply.github.com>
2020-11-15 17:00:28 +0530

    Fixed daily quiz

--- Progress_Reminder_DailyQuiz/homescreen.dart
+++ Progress_Reminder_DailyQuiz/homescreen.dart
@@ -1,273 +1,3 @@
-/*import 'dart:math';
-
-// import 'package:Layout/dailyQuiz.dart';
-// import 'package:Layout/data.dart';
-// import 'package:Layout/environmentalDays_model.dart';
-// import 'package:Layout/flashcard_model.dart';
-// import 'package:Layout/quiz.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'dailyQuiz.dart';
-import 'data.dart';
-import 'environmentalDays_model.dart';
-import 'flashcard_model.dart';
-// import 'quiz.dart';
-
-import 'flashcard.dart';
-import 'flashcardsScreen.dart';
-
-class HomeScreen extends StatefulWidget {
-
-  @override
-  _HomeScreenState createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  void takeDailyQuiz(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (_) {
-      return DailyQuiz();
-    }));
-  }
-
-  int r;
-
-  @override
-  void initState() {
-    super.initState();
-    // this.rind=randomInd();
-    r=randomInd();
-  }
-
-  Widget flashcardOfTheDay() {
-    DateTime today = DateTime.now();
-    for (var environmentalDay in environmentalDays) {
-      if ((environmentalDay["date"] as Map)["month"] == today.month) {
-        if ((environmentalDay["date"] as Map)["day"] == today.day) {
-          return EnvironmentalDayCard(environmentalDay);
-        }
-      }
-    }
-    flashcardModels[r].lastShown = DateTime.now();
-    return Flashcard(flashcardModels[r],r);
-  }
-
-  void showFlashcards(BuildContext context) {
-    Navigator.push(context, MaterialPageRoute(builder: (_) {
-      return FlashcardsScreen(r);
-    }));
-  }
-
-  int randomInd(){
-      int randomIndex = Random().nextInt(flashcardModels.length);
-      while (!flashcardModels[randomIndex].lastShown.isBefore(DateTime.now()
-          .subtract(Duration(days: flashcardModels[randomIndex].frequency)))) {
-        randomIndex = Random().nextInt(flashcardModels.length);
-      }
-      return randomIndex;
-    // return 43;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(
-        0xff2C2E36,
-      ),
-      appBar: AppBar(
-        title: Text("ECO"),
-        centerTitle: true,
-        backgroundColor: const Color(
-          0xff2C2E36,
-        ),
-        elevation: 0,
-        actions: [
-          IconButton(
-            icon: Icon(
-              Icons.account_circle,
-            ),
-            onPressed: () => showFlashcards(context),
-          ),
-        ],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            InkWell(
-              onTap: () => showFlashcards(context),
-              child: flashcardOfTheDay(),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width - 25,
-              height: 80,
-              margin: EdgeInsets.all(5),
-              child: FlatButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                    10,
-                  ),
-                ),
-                color: const Color(0xffD91B5F),
-                onPressed: () => takeDailyQuiz(context),
-                child: Text(
-                  "Take your daily quiz!",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: const Color(0xffFFFFFF),
-                  ),
-                ),
-              ),
-            ),
-            // Card(
-            //   child: SizedBox(
-            //     child: ListView.builder(
-            //       itemBuilder: (context, index) {
-            //         return SizedBox(
-            //           height: 80,
-            //           width: 80,
-            //           child: SvgPicture.asset("assets/Bulkasaur final.svg"),
-            //         );
-            //       },
-            //       itemCount: 4,
-            //     ),
-            //   ),
-            // ),
-            Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.29,
-                        width: MediaQuery.of(context).size.height * 0.29,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              10,
-                            ),
-                          ),
-                          color: const Color(0xffEA8134),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SvgPicture.asset(
-                                "assets/Group 28.svg",
-                              ),
-                              Text(
-                                "WASTE GURU",
-                                style: TextStyle(
-                                  color: const Color(0xffFFFFFF),
-                                  fontSize: 22,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.29,
-                        width: MediaQuery.of(context).size.height * 0.29,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              10,
-                            ),
-                          ),
-                          color: const Color(0xffFF7262),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SvgPicture.asset(
-                                "assets/Group 29.svg",
-                              ),
-                              Text(
-                                "SMART PLUG",
-                                style: TextStyle(
-                                  color: const Color(0xffFFFFFF),
-                                  fontSize: 22,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.29,
-                        width: MediaQuery.of(context).size.height * 0.29,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              10,
-                            ),
-                          ),
-                          color: const Color(0xffFF4F6A),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SvgPicture.asset(
-                                "assets/Group 30.svg",
-                              ),
-                              Text(
-                                "BREATHE IN",
-                                style: TextStyle(
-                                  color: const Color(0xffFFFFFF),
-                                  fontSize: 22,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: MediaQuery.of(context).size.height * 0.29,
-                        width: MediaQuery.of(context).size.height * 0.29,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(
-                              10,
-                            ),
-                          ),
-                          color: const Color(0xffFDB714),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              SvgPicture.asset(
-                                "assets/Group 31.svg",
-                              ),
-                              Text(
-                                "ECOmmunity",
-                                style: TextStyle(
-                                  color: const Color(0xffFFFFFF),
-                                  fontSize: 22,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    );
-    }
-
-}*/
-
 import 'dart:math';
 
 // import 'package:Layout/dailyQuiz.dart';
@@ -284,7 +14,7 @@ import 'data.dart';
 import 'environmentalDays_model.dart';
 import 'flashcard.dart';
 import 'flashcard_model.dart';
-
+import 'question.dart';
 import 'flashcardsScreen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -320,19 +50,16 @@ class _HomeScreenState extends State<HomeScreen> {
     print("$randomIndex");
     if (lastOpenedTime
         .isBefore(DateTime.now().subtract(const Duration(days: 1)))) {
-      print('a');
       int randomIndex = Random().nextInt(10);
       String lastCardShownDateString =
           prefs.getString("lastShown" + randomIndex.toString()) ?? "";
       DateTime lastCardShownDate;
       if (lastCardShownDateString == "") {
-        print('1');
         prefs.setString("lastShown" + randomIndex.toString(),
             DateTime.now().toIso8601String());
         lastCardShownDate = DateTime.parse(
             prefs.getString("lastShown" + randomIndex.toString()));
       } else {
-        print('2');
         lastCardShownDate = DateTime.parse(lastCardShownDateString);
       }
       while (!lastCardShownDate.isBefore(DateTime.now()
@@ -341,9 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
         lastCardShownDate = DateTime.parse(
             prefs.getString("lastShown" + randomIndex.toString()) ??
                 DateTime(2000).toIso8601String());
-        print('b');
       }
-      print('c');
       int temp = prefs.getInt("randomIndex") ?? 0;
       await prefs.setInt("randomIndex", randomIndex);
       temp = randomIndex;
@@ -368,7 +93,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void takeDailyQuiz(BuildContext context) {
     Navigator.push(context, MaterialPageRoute(builder: (_) {
-      return DailyQuiz(randomIndex);
+      return DailyQuiz(0);
     }));
   }
 
@@ -387,7 +112,9 @@ class _HomeScreenState extends State<HomeScreen> {
         }
       }
     }
-    return Flashcard(flashcardModels[randomIndex], randomIndex);
+    // return Flashcard(flashcardModels[randomIndex], randomIndex);
+    int r=randomIndex;
+    return Question(data[r]["question"], data[r]["options"], data[r], 0, flashcardModels[r], r);
   }
 
   @override
