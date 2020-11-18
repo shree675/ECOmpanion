474319490ae087f96ca1237224511d098e687f95
shree675 <58718144+shree675@users.noreply.github.com>
2020-11-18 21:02:30 +0530

    fixed badge and quiz

--- Layout/lib/question2.dart
+++ Layout/lib/question2.dart
@@ -2,22 +2,17 @@ import 'package:flutter/material.dart';
 
 class Question2 extends StatelessWidget {
   final String question;
-  final Color color;
-  Question2(this.question, this.color);
+  int index;
+  Question2(this.question,this.index);
   @override
   Widget build(BuildContext context) {
+    // print(index);
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
       child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        color: color,
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Text(
-              question,
-              style: TextStyle(fontSize: 18, color: const Color(0xffFFFFFF)),
-            ),
+            padding: const EdgeInsets.all(8.0),
+            child: Text(question),
           ),
         ),
       ),
